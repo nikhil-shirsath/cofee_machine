cbf33582209ce21d233f662073f7666f10186758
nikhil shirsath <001nikhilshirsath001@gmail.com>
2023-08-20 12:59:13 +0530

    everything is ok

--- main.py
+++ main.py
@@ -42,15 +42,14 @@ show_again=True
 # c. The same should happen if another resource is depleted, e.g. milk or coffee
 
 def resource_sufficient(drink, resources):
-    # print(drink)
-    # print(resources)
+    """This function takes drink ingredeants and resouces available and returns True if resources are available,
+    otherwise it returns false ."""
     for item in drink :
         if drink[item] > resources[item]:
             print(f"Sorry there is not enough {item}.")
             return False
-        else:
-            is_sufficient=True
-    return is_sufficient
+        
+    return True
 
 
 # #TODO:5  Process coins.
@@ -61,14 +60,34 @@ def resource_sufficient(drink, resources):
 # pennies = 0.25 + 0.1 x 2 + 0.05 + 0.01 x 2 = $0.52
 
 def process_coins():
-    if(resource_sufficient(drink=drink,resources=resources)):
-        print(" \n\nPlease Insert coins \n\n")
-        user_coins =int(input("insert quarters "))
-        user_coins =int(input("insert dimes "))
-        user_coins =int(input("insert nickles "))
-        user_coins =int(input("insert pennies "))
-        
+    """ This function calculates coins and change them into the $ dollers 
+    returns total dollers."""
+    user_total_coins=0
+    print(" \n\nPlease Insert coins \n\n")
+    user_toatal_coins =int(input("insert quarters "))*0.25
+    user_total_coins +=int(input("insert dimes "))*0.10
+    user_total_coins +=int(input("insert nickles "))*0.05
+    user_total_coins +=int(input("insert pennies "))*0.01    
+    return user_total_coins 
 
+def is_transection_successful(money_recieved,drink_cost):
+    """Return true if payment is accepted or return false if payment is denied 
+    and refunds money."""
+    if money_recieved < drink_cost:
+        print("Sorry that's not enough money. Money refunded.")
+        return False
+    else :
+        global profit
+        profit += drink_cost
+        change_money = round(money_recieved - drink_cost,2)
+        print(f"Here is $.{change_money} dollars in change.")
+        return True
+
+
+def make_coffee(coffee_ingredients, resources):
+    for item in coffee_ingredients:
+        resources[item] -=coffee_ingredients[item]
+    return True
 
 
 
@@ -79,8 +98,14 @@ while show_again:
     if user_choice=="off" or user_choice=="OFF":
         show_again=False
     elif user_choice=="report":
-        print(f" water : {resources['water']} \n milk : {resources['milk']} \n coffee : {resources['coffee']} " )
+        print(f" water : {resources['water']} \n milk : {resources['milk']} \n coffee : {resources['coffee']} \n collection ${profit} " )
     else:
         drink =MENU[user_choice]
         # print(drink)
-        print(resource_sufficient(drink=drink['ingredients'],resources=resources))
+        if (resource_sufficient(drink=drink['ingredients'],resources=resources)):
+            money_recieved = process_coins()
+            trasection = is_transection_successful(money_recieved=money_recieved, drink_cost=drink['cost'])
+            if trasection:
+               coffee_done = make_coffee(coffee_ingredients=drink['ingredients'],resources=resources)
+               if coffee_done:
+                   print(f"Here is your {user_choice}. Enjoy!.")
